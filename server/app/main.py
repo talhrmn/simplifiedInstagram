817655dc65c014e5ad5548c9f64e5bbd5db8ea73
talhrmn <talhrmn@gmail.com>
2025-03-13 10:55:00 +0200

    Basic app with redis db

---
+++ server/app/main.py
@@ -0,0 +1,47 @@
+from contextlib import asynccontextmanager
+
+import uvicorn
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import RedirectResponse
+
+from server.app.views.api import api_router
+from server.clients.redis_client import redis_client
+from server.core.settings import server_settings
+from fastapi import FastAPI
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    redis_client.connect()
+    yield
+    redis_client.close()
+
+
+app = FastAPI(
+    title=server_settings.APP_NAME,
+    version=server_settings.API_VERSION,
+    openapi_url=f"{server_settings.API_PREFIX}/openapi.json",
+    docs_url=f"{server_settings.API_PREFIX}/docs",
+    redoc_url=f"{server_settings.API_PREFIX}/redoc",
+    lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=server_settings.CORS_ORIGINS,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+app.include_router(api_router, prefix=f"{server_settings.API_PREFIX}/{server_settings.API_VERSION}")
+
+
+@app.get("/", include_in_schema=False)
+def root():
+    return RedirectResponse(url=f"{server_settings.API_PREFIX}/docs")
+
+
+if __name__ == '__main__':
+    uvicorn.run(app=server_settings.APP_ROUTE, host=server_settings.APP_HOST, port=server_settings.APP_PORT,
+                reload=True)
---
+++ server/core/settings.py
@@ -0,0 +1,29 @@
+from typing import List
+
+from dotenv import load_dotenv
+from pydantic_settings import BaseSettings
+
+load_dotenv()
+
+
+class Settings(BaseSettings):
+    APP_NAME: str = "Simplified Instagram"
+    APP_ROUTE: str
+    APP_HOST: str = "0.0.0.0"
+    APP_PORT: int = 8000
+
+    API_PREFIX: str = "/api"
+    API_VERSION: str = "v1"
+
+    REDIS_HOST: str
+    REDIS_PORT: int
+    REDIS_DB: int
+
+    CORS_ORIGINS: List[str]
+
+    class Config:
+        env_file = ".env"
+        env_file_encoding = "utf-8"
+
+
+server_settings = Settings()
